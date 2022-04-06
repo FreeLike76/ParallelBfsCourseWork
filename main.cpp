dd0c3ddcd7d53fc791c0262c51c56ebce41d3129
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2022-04-06 14:38:03 +0300

    sequentialBFS function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include "stdafx.h"
 
 std::vector<std::vector<int>> graphGen(int size);
+std::vector<int> sequentialBFS(std::vector<std::vector<int>> graph, int from, int to);
+
 
 int main()
 {
 	auto graph = graphGen(250);
-
+	sequentialBFS(graph, 0, 213);
 	std::cout << "End\n";
 }
-
 
 std::vector<std::vector<int>> graphGen(int size)
 {
@@ -23,5 +24,38 @@ std::vector<std::vector<int>> graphGen(int size)
 	}
 
 	return graph;
+}
+
+std::vector<int> sequentialBFS(std::vector<std::vector<int>> graph, int from, int to)
+{
+	std::vector<bool> visited(graph.size(), false);
+	std::queue<int> queue;
+
+	// Starting with [from]
+	visited[from] = true;
+	queue.push(from);
+
+	while (!queue.empty())
+	{
+		// Get the first in queue
+		int cur = queue.front();
+		std::cout << cur << " ";
+		queue.pop();
+		if (cur == to) 
+		{
+			std::cout << "\nFOUND!\n";
+			break;
+		}
+
+		// For every adjacent that is not yet visited - add to queue
+		for (int i = 0; i < graph[cur].size(); i++)
+		{
+			if (graph[cur][i] == 1 && !visited[i])
+			{
+				visited[i] = true;
+				queue.push(i);
+			}
+		}
+	}
 }
 
--- stdafx.h
+++ stdafx.h
@@ -2,4 +2,4 @@
 
 #include <iostream>
 #include <vector>
-#include <numeric>
+#include <queue>
